fc2e34ae61269c6f94de4d9fd9694289a1b4e8d4
celikozcaglar <caglar.celikoz@ida-mediafoundry.be>
2021-09-26 20:07:21 +0200

    created header component + blog and index pages w/ styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,28 @@
 // import App from 'next/app'
-import 'tailwindcss/tailwind.css'
+import "../styles/global.css";
+import Header from "../components/header";
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
-  }
-  
-  // Only uncomment this method if you have blocking data requirements for
-  // every single page in your application. This disables the ability to
-  // perform automatic static optimization, causing every page in your app to
-  // be server-side rendered.
-  //
-  // MyApp.getInitialProps = async (appContext) => {
-  //   // calls page's `getInitialProps` and fills `appProps.pageProps`
-  //   const appProps = await App.getInitialProps(appContext);
-  //
-  //   return { ...appProps }
-  // }
-  
-  export default MyApp
+  return (
+    <div className="antialised bg-gray-100 text-gray-700 h-screen px-2">
+      <Header />
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </div>
+  );
+}
+
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// MyApp.getInitialProps = async (appContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+//
+//   return { ...appProps }
+// }
+
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
 function HomePage() {
-    return <div className="text-purple-500 bg-blue-100">Welcome</div>
+  return (
+    <>
+      <div className="site-container">
+        <div className="space-y-4">
+          <h1 className="text-2xl font-bold">
+            I am Caglar Celiköz, a full stack developer from Belgium
+          </h1>
+        </div>
+      </div>
+
+      <div className="max-w-2xl mx-auto mt-20">
+        <img className="rounded-2xl" src="/images/caglar.png" alt="" />
+      </div>
+    </>
+  );
 }
 
 export default HomePage;
